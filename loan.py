f6e5007b97dd38b175b1cbbdf865ad74c1f6edd6
PallaviDahal <100246102+PallaviDahal@users.noreply.github.com>
2022-02-22 18:02:40 -0600

    Create loan.py

---
+++ loan.py
@@ -0,0 +1,9 @@
+print("Welcome to affnity plus")
+print("--------------------------------------")
+Money=input("   How much money you have? $")
+loan=input("   How much loan you have? $" )
+Money_as_num=int(Money)
+loan_as_num=int(loan)
+Total= Money_as_num - loan_as_num
+print(f"   You will have ${Total} if you pay your loan")
+print("----------------------------------------")
